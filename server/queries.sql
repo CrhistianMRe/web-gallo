707855e00df64c7428578ac2112d9bcc07e1213d
CrhistianMRe <came.29meca@gmail.com>
2025-11-30 12:46:34 +0800

    feat(server): add workout data list retrieve module

--- server/queries.sql
+++ server/queries.sql
@@ -15,3 +15,16 @@ WHERE body_part.name = "Chest"
 -- WHERE body_part.id = 1
 
 
+-- List of workouts with sets, weight amount, to_failure, exercise name, and body part name
+
+SELECT exercise.name AS exercise_name, exercise.image_url, workout.workout_date, workout.workout_length, workout_set.rep_amount, workout_set.weight_amount, workout_set.to_failure, body_part.name AS body_part_name
+FROM exercise
+INNER JOIN exercise_body_part
+ON exercise_body_part.exercise_id = exercise.id
+INNER JOIN body_part
+ON body_part.id = exercise_body_part.body_part_id
+INNER JOIN workout
+ON workout.exercise_id = exercise.id
+INNER JOIN workout_set
+ON workout_set.workout_id = workout.id
+
