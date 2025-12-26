67a7134c4c332105f264fe4c26f3c8d2a8370edb
CrhistianMRe <came.29meca@gmail.com>
2025-12-26 14:13:19 +0800

    feat(android): add workout list view

---
+++ android/app/src/main/java/com/crhistianm/galloalpha/view/persistence/load/WorkoutListScreen.kt
@@ -0,0 +1,127 @@
+package com.crhistianm.galloalpha.view.persistence.load
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.crhistianm.galloalpha.R
+import com.crhistianm.galloalpha.view.core.components.GalloBottomBar
+import com.crhistianm.galloalpha.view.core.components.GalloCard
+import com.crhistianm.galloalpha.view.core.components.GalloFloatingButton
+import com.crhistianm.galloalpha.view.core.components.GalloTopAppBar
+import com.crhistianm.galloalpha.view.core.components.model.NavItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+@Preview
+fun WorkoutListScreen() {
+
+    val listState: LazyListState = rememberLazyListState()
+    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colorStops = arrayOf(
+                        0.2f to Color(0xFF131212),
+                        0.6f to Color.Black,
+                    )
+                )
+            )
+    ) {
+        Scaffold (
+            modifier = Modifier
+                .fillMaxSize(),
+            floatingActionButtonPosition = FabPosition.Center,
+            containerColor = Color.Transparent,
+            contentColor = Color.Transparent,
+            floatingActionButton = {
+                /*GalloFloatingButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add icon image",
+                        modifier = Modifier.size(40.dp)
+                    )
+                }*/
+            },
+            topBar = {
+                GalloTopAppBar(title = "Workout List", navigationIcon = {}) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowUp,
+                        contentDescription = "Calendar image",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .size(39.dp)
+                            .padding(end = 5.dp)
+                            .clickable{
+                                coroutineScope.launch {
+                                    listState.animateScrollToItem(0)
+                                }
+                            }
+                    )
+                }
+            },
+            bottomBar = {
+                GalloBottomBar(
+                    startingIndex = 0,
+                    itemList = listOf (
+                        NavItem(
+                            name = "Workouts",
+                            icon = R.drawable.icon_workout
+                        )
+                    )
+                )
+            }
+
+        ) { innerPadding ->
+            LazyColumn (
+                modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                state = listState,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    GalloCard(
+                        primaryText = "Exercise",
+                        topText = "Date",
+                        bottomText = "Length",
+                        icon = R.drawable.icon_workout
+                    )
+                }
+            }
+
+        }
+    }
+
+
+}
+
+
