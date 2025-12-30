ff1b4af31263eeda46264382b95a7dd9e6bd7ade
CrhistianMRe <came.29meca@gmail.com>
2025-12-30 16:29:55 +0800

    feat(android): add exercise list screen view

---
+++ android/app/src/main/java/com/crhistianm/galloalpha/view/persistence/load/ExerciseListScreen.kt
@@ -0,0 +1,67 @@
+package com.crhistianm.galloalpha.view.persistence.load
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.crhistianm.galloalpha.R
+import com.crhistianm.galloalpha.view.core.components.GalloBottomBar
+import com.crhistianm.galloalpha.view.core.components.GalloCard
+import com.crhistianm.galloalpha.view.core.components.GalloMainLayout
+import com.crhistianm.galloalpha.view.core.components.model.NavItem
+import com.crhistianm.galloalpha.view.core.navigation.Screens
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun ExerciseListScreen (
+) {
+
+    GalloMainLayout(
+        title = "Exercise List",
+        bottomBar = {
+            GalloBottomBar(
+                startingIndex = 1,
+                itemList = listOf (
+                    NavItem(
+                        name = "Workouts",
+                        icon = R.drawable.icon_workout,
+                    ),
+                    NavItem(
+                        name = "Exercises",
+                        icon = R.drawable.icon_exercise
+                    )
+                )
+            )
+        },
+    ) { innerPadding, listState ->
+        LazyColumn (
+            modifier = Modifier.padding(innerPadding),
+            state = listState,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                GalloCard (
+                    modifier = Modifier.fillMaxWidth().height(100.dp),
+                    topText = "Exercise name",
+                    primaryText = "Body parts",
+                    imageUrl = "https://avatars.githubusercontent.com/u/106357444?v=4",
+                )
+            }
+
+        }
+
+    }
+
+
+
+}
